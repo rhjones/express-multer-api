51c473692c17b8627d92e23023639b430f00d8f9
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-13 15:42:43 -0400

    Prepare AWS S3 options
    Define bucket name, attach fileBuffer, choose access policy, define mimetype, and set filename for S3 to use for the upload destination

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -34,12 +34,32 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
-const logMessage = (file) => {
-  console.log(`${filename} is ${file.data.length} bytes long and is of mime ${file.mime}`);
+const upload = (file) => {
+  const options = {
+    // get bucket name from AWS S3 console
+    Bucket: 'rhj-ga-wdi',
+    // attach fileBuffer as stream to send to S3
+    Body: file.data,
+    // allow anyone to view uploaded file
+    ACL: 'public-read',
+    // tell S3 what the mimetype is
+    ContentType: file.mime,
+    // pick a filename for S3 to use for the upload
+    Key: `test/test.${file.ext}`
+  };
+  return Promise.resolve(options);
+};
+
+const logMessage = (upload) => {
+  // get rid of stream for now, so we can cleanly log rest of options in Terminal
+  delete upload.Body;
+  // turn pojo into string for better viewing in console
+  console.log(`the upload options are ${JSON.stringify(upload)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(upload)
 .then(logMessage)
 .catch(console.error)
 ;
